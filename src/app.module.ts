52fb11634f50672300e4de25596516c14bf628b2
Tuan Hiep <tuanhiep@gmail.com>
2024-11-17 14:48:08 +0700

    import env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import { UserEntity } from './entities/user.entity';
 import { UserModule } from './modules/users/user.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { RoleEntity } from './entities/role.entity';
 import { RoleModule } from './modules/roles/role.module';
 import { PositionEntity } from './entities/position.entity';
@@ -48,58 +48,79 @@ import { RecruitmentPostEntity } from './entities/recruitmentPost.entity';
 import { AnnouncementPostModule } from './modules/announcementPosts/announcementPost.module';
 import { AnnouncementPostEntity } from './entities/announcementPost.entity';
 
-// @Module({
-//   imports: [
-//     ConfigModule.forRoot(),
-//     TypeOrmModule.forRoot({
-//       type: 'mysql',
-//       host: process.env.DB_HOST,
-//       port: Number.parseInt(process.env.DB_PORT),
-//       username: process.env.DB_USERNAME,
-//       password: '',
-//       database: process.env.DB_NAME,
-//       entities: [UserEntity],
-//     }),
-//     UserModule,
-//   ],
-//   controllers: [AppController],
-//   providers: [AppService],
-// })
-
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'hospital_hrm_test',
-      entities: [
-        UserEntity,
-        RoleEntity,
-        PositionEntity,
-        AccountEntity,
-        DepartmentEntity,
-        AllowanceEntity,
-        LeaveEntity,
-        UserInsuranceEntity,
-        InsuranceEntity,
-        EvaluateEntity,
-        OvertimeEntity,
-        OvertimeHistoryEntity,
-        LeaveHistoryEntity,
-        MedicalTrainingResultsEntity,
-        NursingTrainingResultsEntity,
-        PositionAllowanceEntity,
-        ContractEntity,
-        ContractHistoryEntity,
-        SalaryHistoryEntity,
-        RecruitmentPostEntity,
-        AnnouncementPostEntity,
-      ],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true, // Tự động load biến môi trường trong toàn bộ ứng dụng
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql', // Hoặc 'postgres', 'sqlite', tùy DB bạn dùng
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [
+          UserEntity,
+          RoleEntity,
+          PositionEntity,
+          AccountEntity,
+          DepartmentEntity,
+          AllowanceEntity,
+          LeaveEntity,
+          UserInsuranceEntity,
+          InsuranceEntity,
+          EvaluateEntity,
+          OvertimeEntity,
+          OvertimeHistoryEntity,
+          LeaveHistoryEntity,
+          MedicalTrainingResultsEntity,
+          NursingTrainingResultsEntity,
+          PositionAllowanceEntity,
+          ContractEntity,
+          ContractHistoryEntity,
+          SalaryHistoryEntity,
+          RecruitmentPostEntity,
+          AnnouncementPostEntity,
+        ],
+        synchronize: true, // Không khuyến nghị bật trong môi trường production
+      }),
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: '',
+    //   database: 'hospital_hrm_test',
+    //   entities: [
+    //     UserEntity,
+    //     RoleEntity,
+    //     PositionEntity,
+    //     AccountEntity,
+    //     DepartmentEntity,
+    //     AllowanceEntity,
+    //     LeaveEntity,
+    //     UserInsuranceEntity,
+    //     InsuranceEntity,
+    //     EvaluateEntity,
+    //     OvertimeEntity,
+    //     OvertimeHistoryEntity,
+    //     LeaveHistoryEntity,
+    //     MedicalTrainingResultsEntity,
+    //     NursingTrainingResultsEntity,
+    //     PositionAllowanceEntity,
+    //     ContractEntity,
+    //     ContractHistoryEntity,
+    //     SalaryHistoryEntity,
+    //     RecruitmentPostEntity,
+    //     AnnouncementPostEntity,
+    //   ],
+    //   synchronize: true,
+    // }),
     UserModule,
     RoleModule,
     PositionModule,
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,19 @@ declare const module: any;
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('APP_PORT');
   app.enableCors({
-    origin: 'http://localhost:5173', // Địa chỉ frontend
+    // origin: 'http://localhost:5173', // Địa chỉ frontend
+    origin: true,
     methods: 'GET,POST,PUT,DELETE',
     credentials: true, // Nếu bạn cần gửi cookie hoặc thông tin xác thực
   });
-  await app.listen(3000);
+  await app.listen(port);
 
   if (module.hot) {
     module.hot.accept();
